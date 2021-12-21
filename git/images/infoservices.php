4f752a1f781bfbcbb6cc216beda7993c067fafa1
visattel <78410730+visattel@users.noreply.github.com>
2021-12-21 22:54:19 +0300

    Add files via upload

---
+++ git/images/infoservices.php
@@ -0,0 +1,110 @@
+﻿<!DOCTYPE html>
+<html class="wide wow-animation scrollTo" lang="en">
+  <head>
+    <title>Информационные услуги</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="keywords" content="intense web design multipurpose template html">
+    <meta name="date" content="Dec 26">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:400,700%7CLato:300,300italic,400,700,900%7CYesteryear">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic">
+    <link rel="stylesheet" href="css/lato.css">
+
+    <link rel="stylesheet" href="css/style.css">
+		<!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+		<![endif]-->
+  </head>
+  <body>
+    <div class="page text-center">
+      <!-- Page Head-->
+      <header class="page-head">
+        <!-- RD Navbar Transparent-->
+        <div class="rd-navbar-wrap">
+          <nav class="rd-navbar rd-navbar-top-panel rd-navbar-light" data-lg-stick-up-offset="79px" data-md-device-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-static" data-lg-auto-height="true" data-md-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-stick-up="true">
+            <div class="container">
+              <div class="rd-navbar-inner">
+                <!--<div class="rd-navbar-top-panel">
+                  <div class="left-side">
+                    <ul class="rd-navbar-top-links list-unstyled">
+                      <li><a class="text-uppercase text-ubold" href="login.html"><small>Log in</small></a></li>
+                      <li><a class="text-uppercase text-ubold" href="register.html"><small>Register</small></a></li>
+                    </ul>
+                  </div>
+                  <div class="right-side">
+                    <address class="contact-info text-left"><span class="p"><span class="novi-icon icon mdi mdi-email-open"></span><a href="mailto:#">info@demolink.org</a></span><span class="p"><span class="novi-icon icon mdi mdi-phone"></span><a href="tel:#">1-800-1234-567</a></span></address>
+                  </div>
+                </div>
+                <!-- RD Navbar Panel-->
+                <div class="rd-navbar-panel">
+                  <!-- RD Navbar Toggle-->
+                  <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar, .rd-navbar-nav-wrap"><span></span></button>
+                  <!-- RD Navbar Top Panel Toggle-->
+                  <!-- <button class="rd-navbar-top-panel-toggle" data-rd-navbar-toggle=".rd-navbar, .rd-navbar-top-panel"><span></span></button>-->
+                  <!--Navbar Brand-->
+                  <div class="rd-navbar-brand"><?php include $_SERVER["DOCUMENT_ROOT"]."/include/logo.php";?></div>
+                </div>
+                <div class="rd-navbar-menu-wrap vstmenumargin">
+<div class="rd-navbar-nav-wrap-bg vstbrd">
+                  <div class="rd-navbar-nav-wrap-bg-inner novi-background vsat"></div>
+</div>
+                  <div class="rd-navbar-nav-wrap">
+                    <div class="rd-navbar-mobile-scroll">
+                      <!--Navbar Brand Mobile-->
+                      <div class="rd-navbar-mobile-brand"><a href="index.html"><img style='margin-top: -5px;margin-left: -15px;' width='105' height='52' src='images/vtel_logo.png' alt=''/></a></div>
+
+<?php include $_SERVER["DOCUMENT_ROOT"]."/include/topmenu.php";?>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+
+
+      <!-- About us-->
+      <section class="section novi-background bg-cover bg-image section-lg bg-default" style="padding-top:100px; padding-bottom: 100px" id="about-us" data-type="anchor">
+        <div class="container">
+	<h1>Информационные услуги</h1>
+          <hr class="divider bg-mantis">
+          <div class="row row-55">
+            <div class="col-md-12 text-left">
+
+<p align="justify">АО "ВИСАТ-ТЕЛ", используя информационный ресурс спутника-ретранслятора, реализует сеть VSAT малого формата в составе центрального узла и абонентских терминалов. Для организации VSAT сети используется оборудование Узла связи АО «ВИСАТ-ТЕЛ» в составе антенной системы 3,7 м с передатчиком 40 Вт.
+	Абонентские терминалы оснащаются антеннами 0,74 – 0,9 м и передатчиками 2 Вт.
+<p align="justify">Предприятиям - клиентам сети, помимо доступа в интернет, предлагается для реализации на объектах:
+<img src="images/infoservices.jpg" alt="Information services" width="100%" height="auto" style="display: block; margin:0 auto"/>
+        <h5 class="font-weight-bold text-primary offset-top-20" align="center">Контроль соблюдения расписания движения общественного транспорта</h5>  
+<img src="images/transportservices.jpg" alt="Information services" width="100%" height="auto" style="display: block; margin:0 auto"/>  
+<hr class="divider bg-mantis"> 
+<h5 class="font-weight-bold text-primary offset-top-20" align="center">Система контроля и управления доступом</h5>
+<img src="images/skudservices.jpg" alt="Information services" width="100%" height="auto" style="display: block; margin:0 auto"/> 
+<hr class="divider bg-mantis">
+<h5 class="font-weight-bold text-primary offset-top-20" align="center">Индивидуальный универсальный ключ к любым видам платных услуг</h5>
+<hr class="divider bg-mantis">
+<h5 class="font-weight-bold text-primary offset-top-20" align="center">Программно-аппаратный комплекс OnLine заказа и оплаты топливозаправки</h5>
+<hr class="divider bg-mantis">
+<h5 class="font-weight-bold text-primary offset-top-20" align="center">Контроль наличия и перемещения транспорта и грузов</h5>
+<hr class="divider bg-mantis">
+<h5 class="font-weight-bold text-primary offset-top-20" align="center">Услуги ЦОД:<br>IaaS – инфраструктура как услуга,<br>PaaS – платформа как услуга,<br>SaaS – программное обеспечение как услуга</h5>
+         </div>
+
+        </div>
+      </section>
+
+<?php include $_SERVER["DOCUMENT_ROOT"]."/include/footer.php";?>
+
+    </div>
+    <!-- Global RD Mailform Output -->
+    <div class="snackbars" id="form-output-global"></div>
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
---
+++ git/images/management.php
@@ -0,0 +1,278 @@
+﻿<!DOCTYPE html>
+<html class="wide wow-animation scrollTo" lang="en">
+  <head>
+    <title>Руководство</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="keywords" content="intense web design multipurpose template html">
+    <meta name="date" content="Dec 26">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:400,700%7CLato:300,300italic,400,700,900%7CYesteryear">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic">
+    <link rel="stylesheet" href="css/lato.css">
+
+    <link rel="stylesheet" href="css/style.css">
+		<!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+		<![endif]-->
+  </head>
+  <body>
+<!--    <div class="page-loader page-loader-variant-1">
+      <div><img class='img-fluid' style='margin-top: -20px;margin-left: -18px;' width='330' height='67' src='images/intense/logo-big.png' alt=''/>
+        <div class="offset-top-41 text-center">
+          <div class="spinner"></div>
+        </div>
+      </div>
+    </div>-->
+    <div class="page text-center">
+      <!-- Page Head-->
+
+
+
+      <header class="page-head">
+        <!-- RD Navbar Transparent-->
+        <div class="rd-navbar-wrap">
+          <nav class="rd-navbar rd-navbar-top-panel rd-navbar-light" data-lg-stick-up-offset="79px" data-md-device-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-static" data-lg-auto-height="true" data-md-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-stick-up="true">
+            <div class="container">
+              <div class="rd-navbar-inner">
+                <!--<div class="rd-navbar-top-panel">
+                  <div class="left-side">
+                    <ul class="rd-navbar-top-links list-unstyled">
+                      <li><a class="text-uppercase text-ubold" href="login.html"><small>Log in</small></a></li>
+                      <li><a class="text-uppercase text-ubold" href="register.html"><small>Register</small></a></li>
+                    </ul>
+                  </div>
+                  <div class="right-side">
+                    <address class="contact-info text-left"><span class="p"><span class="novi-icon icon mdi mdi-email-open"></span><a href="mailto:#">info@demolink.org</a></span><span class="p"><span class="novi-icon icon mdi mdi-phone"></span><a href="tel:#">1-800-1234-567</a></span></address>
+                  </div>
+                </div>
+                <!-- RD Navbar Panel-->
+                <div class="rd-navbar-panel">
+                  <!-- RD Navbar Toggle-->
+                  <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar, .rd-navbar-nav-wrap"><span></span></button>
+                  <!-- RD Navbar Top Panel Toggle-->
+                  <button class="rd-navbar-top-panel-toggle" data-rd-navbar-toggle=".rd-navbar, .rd-navbar-top-panel"><span></span></button>
+                  <!--Navbar Brand-->
+
+                  <div class="rd-navbar-brand"><?php include $_SERVER["DOCUMENT_ROOT"]."/include/logo.php";?></div>
+                </div>
+
+                <div class="rd-navbar-menu-wrap vstmenumargin">
+<div class="rd-navbar-nav-wrap-bg vstbrd">
+                  <div class="rd-navbar-nav-wrap-bg-inner novi-background vsat"></div>
+</div>
+
+                  <div class="rd-navbar-nav-wrap">
+
+
+                    <div class="rd-navbar-mobile-scroll">
+
+
+                      <!--Navbar Brand Mobile-->
+                      <div class="rd-navbar-mobile-brand"><a href="index.php"><img style='margin-top: -5px;margin-left: -15px;' width='75' height='75' src='images/mobileicon.ico' alt=''/></a></div>
+
+
+
+<?php include $_SERVER["DOCUMENT_ROOT"]."/include/topmenu.php";?>
+
+
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+
+
+      <!-- Classic Breadcrumbs-->
+
+      <section class="section novi-background breadcrumb-classic" style="margin-top:50px;" >
+        <div class="container section-34 section-sm-50">
+          <div class="row align-items-xl-center">
+            <div class="col-xl-5 d-none d-xl-block text-xl-left">
+              <h2><span class="">Руководство</span></h2>
+            </div>
+            <!-- <div class="col-xl-2 d-none d-md-block"><span class="icon icon-white mdi mdi-account-box-outline"></span></div>-->
+            <div class="offset-top-0 offset-md-top-10 col-xl-5 offset-xl-top-0 small text-xl-right">
+              <ul class="list-inline list-inline-dashed p">
+                <li class="list-inline-item"><a href="index.php">Главная</a></li>
+                <li class="list-inline-item"><a href="about.php">Наша компания</a></li>
+                <li class="list-inline-item">Руководство</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+            <svg class="svg-triangle-bottom" xmlns="http://www.w3.org/2000/svg" version="1.1">
+              <defs>
+                <lineargradient id="grad2" x1="0%" y1="0%" x2="100%" y2="0%">
+                  <stop offset="0%" style="stop-color:rgb(58,125,181);stop-opacity:1"></stop>
+                  <stop offset="100%" style="stop-color:rgb(59,126,182);stop-opacity:1"></stop>
+                </lineargradient>
+              </defs>
+              <polyline points="0,0 60,0 29,29" fill="url(#grad2)"></polyline>
+            </svg>
+      </section>
+
+
+
+      <section class="section novi-background bg-cover bg-image section-lg bg-default" style="padding-top:100px; padding-bottom: 100px" id="about-us" data-type="anchor">
+
+      <!--<section class="section novi-background bg-cover bg-image section-lg bg-default">-->
+
+<!--      <section class="section novi-background section-98 section-sm-110 section-bottom-66">-->
+        <div class="container">
+          <!--<h2 class="font-weight-bold">Company</h2>-->
+          <!--<hr class="divider bg-mantis">-->
+          <div>
+                  <!-- Responsive-tabs-->
+                  <div class="responsive-tabs responsive-tabs-classic" data-type="horizontal">
+                    <!--<ul class="resp-tabs-list tabs-1 text-center tabs-group-default" data-group="tabs-group-default">
+                      <li>Overview</li>
+                      <li>Soul of agency </li>
+                      <li>Team</li>
+                      <li>Testimonials</li>
+                    </ul>-->
+                    <div class="resp-tabs-container text-left tabs-group-default" data-group="tabs-group-default">
+                      <div class="text-center text-lg-left">
+                        <div class="row justify-content-sm-center section-34">
+
+                          <div class="col-md-6 col-lg-3"><img class="img-fluid d-inline-block" src="images/users/man.png" width="270" height="270" alt="">
+                            <div class="text-lg-left offset-top-20">
+                              <div>
+                                <h5 class="font-weight-bold text-primary" align="center">ЭЙДУС<br> АЛЕКСАНДР<br> ГИРШЕВИЧ</h5>
+				<p align="center">Генеральный директор
+                              </div>
+                            </div>
+                            <!--<address class="contact-info text-lg-left">
+                              <ul class="list-unstyled p">
+                                <li class="d-block"><span class="icon icon-xxs novi-icon mdi mdi-phone text-middle"></span> <a class="d-inline-block text-middle" href="tel:1-800-7650-986">Генеральный директор</a>
+                                </li>
+                                <li class="d-block"><span class="icon icon-xxs novi-icon mdi mdi-email-open text-middle"></span> <a class="d-inline-block text-middle" href="mailto:alex@demolink.org">alex@demolink.org</a>
+                                </li>
+                              </ul>
+                            </address>-->
+                          </div>
+
+                          <div class="col-md-6 col-lg-3 offset-top-50 offset-md-top-0"><img class="img-fluid d-inline-block" src="images/users/man.png" width="270" height="270" alt="">
+                            <div class="text-lg-left offset-top-20">
+                              <div>
+                                <h5 class="font-weight-bold text-primary" align="center">АНПИЛОГОВ ВАЛЕНТИН РОМАНОВИЧ</h5>
+				<p align="center">Заместитель генерального директора
+                              </div>
+                            </div>
+                            <!--<address class="contact-info text-lg-left">
+                              <ul class="list-unstyled p">
+                                <li class="d-block"><span class="icon icon-xxs novi-icon mdi mdi-phone text-middle"></span> <a class="d-inline-block text-middle" href="tel:1-800-7650-986">1-800-7650-986</a>
+                                </li>
+                                <li class="d-block"><span class="icon icon-xxs novi-icon mdi mdi-email-open text-middle"></span> <a class="d-inline-block text-middle" href="mailto:amanda@demolink.org">amanda@demolink.org</a>
+                                </li>
+                              </ul>
+                            </address>-->
+                          </div>
+
+                          <div class="col-md-6 col-lg-3 offset-top-50 offset-lg-top-0"><img class="img-fluid d-inline-block" src="images/users/man.png" width="270" height="270" alt="">
+                            <div class="text-lg-left offset-top-20">
+                              <div>
+                                <h5 class="font-weight-bold text-primary" align="center">ТЫРИН<br> ПАВЕЛ <br>МИХАЙЛОВИЧ</h5>
+				<p align="center">Заместитель генерального директора
+                              </div>
+                            </div>
+                            <!--<address class="contact-info text-lg-left">
+                              <ul class="list-unstyled p">
+                                <li class="d-block"><span class="icon icon-xxs novi-icon mdi mdi-phone text-middle"></span> <a class="d-inline-block text-middle" href="tel:1-800-7650-986">1-800-7650-986</a>
+                                </li>
+                                <li class="d-block"><span class="icon icon-xxs novi-icon mdi mdi-email-open text-middle"></span> <a class="d-inline-block text-middle" href="mailto:bernard@demolink.org">bernard@demolink.org</a>
+                                </li>
+                              </ul>
+                            </address>-->
+                          </div>
+		<div class="col-md-6 col-lg-3 offset-top-50 offset-lg-top-0"><img class="img-fluid d-inline-block" src="images/users/man.png" width="270" height="270" alt="">
+                            <div class="text-lg-left offset-top-20">
+                              <div>
+                                <h5 class="font-weight-bold text-primary" align="center">УВАРОВ<br> ВЛАДИМИР<br> НИКОЛАЕВИЧ</h5>
+				<p align="center">Директор Департамента развития центральных узлов сетей спутниковой связи
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-md-6 col-lg-3 offset-top-50 offset-lg-top-0"><img class="img-fluid d-inline-block" src="images/users/man.png" width="270" height="270" alt="">
+                            <div class="text-lg-left offset-top-20">
+                              <div>
+                                <h5 class="font-weight-bold text-primary" align="center">КУТАСЕВИЧ<br> ВЛАДИМИР<br> МИХАЙЛОВИЧ</h5>
+				<p align="center">Директор Департамента развертывания станций и сетей спутниковой связи
+                              </div>
+                            </div>
+                            <!--<address class="contact-info text-lg-left">
+                              <ul class="list-unstyled p">
+                                <li><span class="icon icon-xxs novi-icon mdi mdi-phone text-middle"></span> <a class="d-inline-block text-middle" href="tel:1-800-7650-986">1-800-7650-986</a>
+                                </li>
+                                <li><span class="icon icon-xxs novi-icon mdi mdi-email-open text-middle"></span> <a class="d-inline-block text-middle" href="mailto:diana@demolink.org">diana@demolink.org</a>
+                                </li>
+                              </ul>
+                            </address>-->
+                          </div>
+
+                          
+                          <div class="col-md-6 col-lg-3 offset-top-50 offset-lg-top-0"><img class="img-fluid d-inline-block" src="images/users/man.png" width="270" height="270" alt="">
+                            <div class="text-lg-left offset-top-20">
+                              <div>
+                                <h5 class="font-weight-bold text-primary" align="center">ТОРСУКОВ<br> ЮРИЙ<br> СЕРГЕЕВИЧ</h5>
+				<p align="center">Директор департамента логистики и строительства
+                              </div>
+                            </div>
+                            <!--<address class="contact-info text-lg-left">
+                              <ul class="list-unstyled p">
+                                <li><span class="icon icon-xxs novi-icon mdi mdi-phone text-middle"></span> <a class="d-inline-block text-middle" href="tel:1-800-7650-986">1-800-7650-986</a>
+                                </li>
+                                <li><span class="icon icon-xxs novi-icon mdi mdi-email-open text-middle"></span> <a class="d-inline-block text-middle" href="mailto:diana@demolink.org">diana@demolink.org</a>
+                                </li>
+                              </ul>
+                            </address>-->
+                          </div>
+                          <div class="col-md-6 col-lg-3 offset-top-50 offset-lg-top-0"><img class="img-fluid d-inline-block" src="images/users/man.png" width="270" height="270" alt="">
+                            <div class="text-lg-left offset-top-20">
+                              <div>
+                                <h5 class="font-weight-bold text-primary" align="center">ЧИСТИК<br> АЛЕКСАНДР<br> ВИКЕНТЬЕВИЧ</h5>
+				<p align="center">Директор производственно-технологического центра
+                              </div>
+                            </div>
+                            <!--<address class="contact-info text-lg-left">
+                              <ul class="list-unstyled p">
+                                <li><span class="icon icon-xxs novi-icon mdi mdi-phone text-middle"></span> <a class="d-inline-block text-middle" href="tel:1-800-7650-986">1-800-7650-986</a>
+                                </li>
+                                <li><span class="icon icon-xxs novi-icon mdi mdi-email-open text-middle"></span> <a class="d-inline-block text-middle" href="mailto:diana@demolink.org">diana@demolink.org</a>
+                                </li>
+                              </ul>
+                            </address>-->
+                          </div>
+
+                        </div>
+                      </div>
+                      
+                      
+                    </div>
+                  </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+<?php include $_SERVER["DOCUMENT_ROOT"]."/include/footer.php";?>
+
+
+
+    </div>
+    <!-- Global RD Mailform Output -->
+    <div class="snackbars" id="form-output-global"></div>
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
